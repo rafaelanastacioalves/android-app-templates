e6072415ca85f978daab1ddf17eed99b20bd30cc
rafaelanastacioalves <rafaelanastacioalves@gmail.com>
2018-05-03 03:08:56 -0300

    refactor: Migra LiveDataMainEntityListViewModel para Kotlin

---
+++ app/src/main/java/com/example/rafaelanastacioalves/moby/entitymainlisting/LiveDataMainEntityListViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.rafaelanastacioalves.moby.entitymainlisting;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.example.rafaelanastacioalves.moby.entities.MainEntity;
+import com.example.rafaelanastacioalves.moby.retrofit.APIClient;
+import com.example.rafaelanastacioalves.moby.retrofit.ServiceGenerator;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import timber.log.Timber;
+
+class LiveDataMainEntityListViewModel : ViewModel() {
+
+    var mainEntityList: MutableLiveData<List<MainEntity>> = MutableLiveData()
+
+
+    fun loadData() {
+        Timber.i("LiveDataMainEntityListViewModel loadData");
+
+        if(mainEntityList.getValue() != null){
+            return;
+        }
+
+        var APIClient: APIClient = ServiceGenerator.createService(APIClient::class.java);
+        var call: Call<List<MainEntity>> = APIClient.getTripPackageList();
+        call.enqueue(object : Callback<List<MainEntity>> {
+
+            override fun onResponse(call: Call<List<MainEntity>>, response: Response<List<MainEntity>>) {
+                if (response.isSuccessful()) {
+                    Timber.i("response Successful");
+                    mainEntityList.setValue(response.body());
+                }
+            }
+
+            override fun onFailure(call: Call<List<MainEntity>>, t: Throwable) {
+                t.printStackTrace();
+            }
+        })
+
+    }
+}
