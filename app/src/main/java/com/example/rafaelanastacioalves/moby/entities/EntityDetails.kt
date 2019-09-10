a9f15e21d643c0bd1aceaeed1d50f7fcedef2348
rafaelanastacioalves <rafaelanastacioalves@gmail.com>
2019-09-10 01:42:52 -0300

    fix: Corrects build

--- app/src/main/java/com/example/rafaelanastacioalves/moby/entities/EntityDetails.kt
+++ app/src/main/java/com/example/rafaelanastacioalves/moby/entities/EntityDetails.kt
@@ -3,25 +3,9 @@ package com.example.rafaelanastacioalves.moby.entities;
 
 class EntityDetails {
 
-    private lateinit var title: String
-    private lateinit var description: String
-    private lateinit var price: String
-    private lateinit var image_url: String
+    public lateinit var title: String
+    public lateinit var description: String
+    public lateinit var price: String
+    public lateinit var image_url: String
 
-
-    fun getTitle(): String {
-        return title;
-    }
-
-    fun getDescription(): String {
-        return description;
-    }
-
-    fun getPrice(): String {
-        return price;
-    }
-
-    fun getImage_url(): String {
-        return image_url;
-    }
 }
